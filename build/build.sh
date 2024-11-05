f97799e92e35edb493851f0afd77233a2f658c3e
Wesley Haines <wes@weshaines.com>
2024-11-04 21:54:48 -0800

    Add autobuild scripts for cloud-init

---
+++ build/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+dnf -y groupinstall 'Development Tools' && dnf -y install zlib-devel libpng-devel minizip-devel libxml2-devel tcl-devel tk-devel
+#mkdir degrib && cd degrib
+#wget https://lamp.mdl.nws.noaa.gov/lamp/Data/degrib/download/degrib-src.tar.gz
+git clone https://github.com/wxUSA/degrib
+cd degrib/src
+./configure CFLAGS="-O3" TCL_PREFIX=/usr TCL_VERSION=$(echo 'puts $tcl_version;'|tclsh)
+make 2>&1 | tee make.out
