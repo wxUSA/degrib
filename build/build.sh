22a7f37246036ecf120b7b7eca18915f07b02145
Wesley Haines <wes@weshaines.com>
2024-11-04 22:34:08 -0800

    Fixup package install order in autobuilder

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #
-dnf -y groupinstall 'Development Tools' && dnf -y install zlib-devel libpng-devel minizip-devel libxml2-devel tcl-devel tk-devel
+dnf -y groupinstall 'Development Tools'
+dnf -y install epel-release
+crb enable
+dnf -y install zlib-devel libpng-devel minizip-devel libxml2-devel tcl tcl-devel tk-devel
 #mkdir degrib && cd degrib
 #wget https://lamp.mdl.nws.noaa.gov/lamp/Data/degrib/download/degrib-src.tar.gz
 git clone https://github.com/wxUSA/degrib
